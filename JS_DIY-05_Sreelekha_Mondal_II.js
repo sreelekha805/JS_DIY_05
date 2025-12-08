6e7b7095587cc9d61ebd2a596b41661dc1cfc9de
Sreelekha Mondal <sreelekhamondal0@gmail.com>
2025-12-08 21:17:47 +0530

    Creating the function dispEachName to display all the values of a perticular property (name) of an array of object instead of using forEach method. And creating the function studentsAbove90 to check the grade is above 90 or not for each student instead of using the filter method

--- JS_DIY-05_Sreelekha_Mondal_II.js
+++ JS_DIY-05_Sreelekha_Mondal_II.js
@@ -1,6 +1,9 @@
 // Creating the object.
 const students = [{name: "Koyel", age: "17", grade: "60"}, {name: "Rupsweta", age: "18", grade: "95"}, {name: "Arunima", age: "18", grade: "87"}, {name: "Mrittika", age: "19", grade: "92"}, {name: "Sanchita", age: "20", grade: "90"}];
 
+let size = students.length;
+console.log (size);
+
 // before displaying the names this is the heading releted to the work.
 document.getElementById("heading1").innerHTML = "The names of all students";
 
@@ -8,24 +11,72 @@ document.getElementById("heading1").innerHTML = "The names of all students";
 const displayName = document.getElementById("display_name");
 
 // Displaying the names of the students in the web page.
-students.forEach (function (student)
+function dispEachName (list, arr)
 {
-  // Creating the p elements to display the anmes individually.
-  const eachName = document.createElement("p");
+  // At first calculating the length of the array of object for deciding how many time the loop will be run.
+  let size = arr.length;
 
-  //Adding the content to each of the p elements.
-  eachName.textContent = student.name;
-  displayName.appendChild(eachName);
-})
+  // Initialized the loop counter.
+  let m = 0;
+  while (m < size)
+  {
+    // To display each name seperately, different <p> element is created dynamically.
+    const para = document.createElement("p");
+
+    // Placing the single name in every paragraph tag.
+    console.log (arr[m]);
+    para.innerHTML = arr[m].name;
+
+    // This <p> elements are the child element of a html element which is accessed by the variable 'list'.
+    list.appendChild(para);
+
+    // The loop counter is incremented.
+    m = (m + 1);
+  }
+}
+
+// Accessing the html element, where the names will be displayed.
+let parentElement = document.getElementById("display_name");
+
+// Calling the function with the html element and the array of object.
+dispEachName (parentElement, students);
 
 // before displaying the all property of students with grade > 90, this is the heading releted to the work.
 document.getElementById("heading2").innerHTML = "The Students with grade > 90 - ";
 
 // Seperating the students according to the condition of grade using filter method.
-const searchWithGrade = students.filter(student => student.grade > 90);
+function studentsAbove90 (list, arr)
+{
+  // At first calculating the length of the array of object for deciding how many time the loop will be run.
+  let size = arr.length;
 
-// Displaying the students all property.
-document.getElementById("grade").innerHTML = JSON.stringify(searchWithGrade);
+  // Initialized the loop counter.
+  let k = 0;
+  while (k < size)
+  {
+    if ((arr[k].grade) > 90)
+    {
+      // To display each name seperately, different <p> element is created dynamically.
+      const para = document.createElement("p");
+
+      // Placing the single name in every paragraph tag.
+      console.log (arr[k]);
+      para.innerHTML = JSON.stringify(arr[k]);
+
+      // This <p> elements are the child element of a html element which is accessed by the variable 'list'.
+      list.appendChild(para);
+    }
+
+    // The loop counter is incremented.
+    k = (k + 1);
+  }
+}
+
+// Accessing the html element, where the names will be displayed.
+let withGrade = document.getElementById("grade");
+
+// Calling the function with the html element and the array of object.
+studentsAbove90 (withGrade, students);
 
 // before displaying the students array of object, this is the heading releted to the work.
 document.getElementById("heading3").innerHTML = "After adding a new student - ";
